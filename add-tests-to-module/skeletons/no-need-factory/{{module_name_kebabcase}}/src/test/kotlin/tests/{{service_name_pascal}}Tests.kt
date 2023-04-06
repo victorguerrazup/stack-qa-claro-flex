6faf0b5a460032ccbceaa76a57d4af504c635afc
Victor Guerra <victor.guerra@zup.com.br>
2023-04-06 18:18:59 -0300

    plugin to add tests to module

---
+++ add-tests-to-module/skeletons/no-need-factory/{{module_name_kebabcase}}/src/test/kotlin/tests/{{service_name_pascal}}Tests.kt
@@ -0,0 +1,20 @@
+package tests
+
+import apis.{{service_name_pascal}}
+import apis.Base
+import org.junit.jupiter.api.Test
+import utils.Mass
+import validations.{{service_name_pascal}}Validate
+
+class {{service_name_pascal}}Tests : Base() {
+
+    private val {{service_name_camelcase}} = {{service_name_pascal}}()
+    private val {{service_name_camelcase}}Validate= {{service_name_pascal}}Validate()
+    private val mass = Mass()
+
+    @Test
+    fun my_test() {
+        val response =  {{service_name_camelcase}}.get()
+        {{service_name_camelcase}}Validate.responseSuccess(response)
+    }
+}
