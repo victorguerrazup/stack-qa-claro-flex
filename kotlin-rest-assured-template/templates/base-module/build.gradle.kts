5ee848ead1c5e155fcfbf76044d6355b5ff84bcf
Victor Guerra <victor.guerra@zup.com.br>
2023-05-03 15:38:33 -0300

    Add faker no template

--- kotlin-rest-assured-template/templates/base-module/build.gradle.kts
+++ kotlin-rest-assured-template/templates/base-module/build.gradle.kts
@@ -7,6 +7,7 @@ dependencies {
     implementation("com.google.code.gson:gson:2.10")
     implementation(platform("org.junit:junit-bom:5.9.+"))
     implementation("org.junit.jupiter:junit-jupiter")
+    implementation("net.datafaker:datafaker:1.9.0")
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
--- kotlin-rest-assured-template/templates/base-module/src/main/kotlin/utils/Mass.kt
+++ kotlin-rest-assured-template/templates/base-module/src/main/kotlin/utils/Mass.kt
@@ -1,41 +1,18 @@
 package utils
 
+import net.datafaker.Faker
 import java.util.*
 import kotlin.math.floor
 import kotlin.math.roundToInt
 
 class Mass {
 
+    private var faker = Faker()
+
     private fun ClosedRange<Int>.random() = Random().nextInt(endInclusive - start) + start
 
-    private fun mod(dividend:Int, divider:Int) : Int {
-        return ((dividend - (floor((dividend / divider).toDouble()) * divider)).roundToInt())
-    }
-
     fun randomCPF() : String {
-        val n1 = (0..9).random()
-        val n2 = (0..9).random()
-        val n3 = (0..9).random()
-        val n4 = (0..9).random()
-        val n5 = (0..9).random()
-        val n6 = (0..9).random()
-        val n7 = (0..9).random()
-        val n8 = (0..9).random()
-        val n9 = (0..9).random()
-        var d1 = n9 * 2 + n8 * 3 + n7 * 4 + n6 * 5 + n5 * 6 + n4 * 7 + n3 * 8 + n2 * 9 + n1 * 10
-
-        d1 = 11 - (mod(d1, 11))
-        if (d1 >= 10){
-            d1 = 0
-        }
-
-        var d2 = d1 * 2 + n9 * 3 + n8 * 4 + n7 * 5 + n6 * 6 + n5 * 7 + n4 * 8 + n3 * 9 + n2 * 10 + n1 * 11
-        d2 = 11 - (mod(d2, 11))
-        if (d2 >= 10){
-            d2 = 0
-        }
-
-        return "" + n1 + n2 + n3 + n4 + n5 + n6 + n7 + n8 + n9 + d1 + d2
+        return faker.cpf().valid(false)
     }
 
     fun randomNumber(length:Int): String {
@@ -58,4 +35,25 @@ class Mass {
         return res
     }
 
+    fun randomName(): String {
+        return faker.name().fullName()
+    }
+
+    fun randomCreditCard(): CreditCard {
+        var creditCard = CreditCard()
+        creditCard.number = faker.business().creditCardNumber().replace("-", "")
+        var expiry = faker.business().creditCardExpiry().split("-")
+        creditCard.expiryMonth = expiry[1]
+        creditCard.expiryYear = expiry[0]
+        creditCard.securityCode = faker.business().securityCode()
+        return creditCard
+    }
+
+}
+
+class CreditCard {
+    var number: String? = null
+    var expiryMonth: String? = null
+    var expiryYear: String? = null
+    var securityCode: String? = null
 }
